f694cfa68c0ac25e87922c416d3466e8fb16edee
thejakha <73944476+thejakha@users.noreply.github.com>
2021-06-23 17:02:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let wrapper = document.getElementById('wrapper')
+let controllers = document.getElementsByClassName('controller')
+let next = document.getElementById('next'), prev = document.getElementById('prev'), right = 0
+    function nextPrev(){
+        next.addEventListener('click',()=>{
+        if (right == 80) {
+            right = 0
+        }else{
+            right += 20
+        }
+        wrapper.style.transform = `translateX(-${right}%)`
+        wrapper.style.transition = '.4s ease-in-out'
+        })
+        prev.addEventListener('click',()=>{
+            if (right == 0) {
+                right = 80
+            }else{
+                right -= 20
+            }
+            wrapper.style.transform = `translateX(-${right}%)`
+        })
+
+        setInterval(()=>{
+            right += 20
+            wrapper.style.transform = `translateX(-${right}%)`
+            wrapper.style.transition = '.4s ease-in-out'
+            if (right == 80) {
+                right = 0
+            }
+        },7000)
+        
+}
+nextPrev()
