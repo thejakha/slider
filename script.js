db6970ba5cb7715fb3b5c179113a5f50a10ed5f5
thejakha <73944476+thejakha@users.noreply.github.com>
2021-06-30 05:16:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let controllers = document.getElementsByClassName('controller')
 let next = document.getElementById('next'), prev = document.getElementById('prev'), right = 0
     function nextPrev(){
         next.addEventListener('click',()=>{
-        if (right == 80) {
+        if (right >= 80) {
             right = 0
         }else{
             right += 20
@@ -12,22 +12,13 @@ let next = document.getElementById('next'), prev = document.getElementById('prev
         wrapper.style.transition = '.4s ease-in-out'
         })
         prev.addEventListener('click',()=>{
-            if (right == 0) {
+            if (right <= 0) {
                 right = 80
             }else{
                 right -= 20
             }
             wrapper.style.transform = `translateX(-${right}%)`
-        })
-
-        setInterval(()=>{
-            right += 20
-            wrapper.style.transform = `translateX(-${right}%)`
-            wrapper.style.transition = '.4s ease-in-out'
-            if (right == 80) {
-                right = 0
-            }
-        },7000)
+        })       
         
 }
 nextPrev()
